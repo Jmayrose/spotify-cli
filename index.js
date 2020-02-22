76a2a3285d2fa7aa9ef66adf4b40588d19819491
Joe <joseph.mayrose@gmail.com>
2020-02-22 18:14:14 -0500

    create commander boilerplate

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const makeSpotify = require("./spotify");
+const commander = require('commander');
+const program = new commander.Command();
 
 let Spotify = makeSpotify();
+program.version("0.0.1");
+
+  program
+    .command("play")
+    .option("-p")
+    .action(function(){
+        
+    });
+  program
+    .command("username <user>")
+    .option("-user")
+    .action(function() {
+    });
+
+  program.parse(process.argv);
