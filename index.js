80f28b12f343640ccb264b673df8e4775a6dbd46
Joe <joseph.mayrose@gmail.com>
2020-02-26 18:08:18 -0500

    add jest dependencies, clean index.js

--- index.js
+++ index.js
@@ -1,59 +1,37 @@
-#!/usr/bin/env node
-
 let SpotifyApi = require("spotify-web-api-node");
-const commander = require("commander");
-const program = new commander.Command();
+const program = require("commander");
 require("dotenv").config();
+
+let Spotify = new SpotifyApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
 
 program.version("0.0.1");
 
-let Spotify = new SpotifyApi({
-  // clientId: process.env.CLIENT_ID,
-  // clientSecret: process.env.CLIENT_SECRET,
-  accessToken: 'BQArN9qzIy22e0XsypudLSh5u_2eG2ICCY_Bkbn4gWrVfCazp6hXx2wIjAtU6g5cGaHRmq3ZuFLtlmcDNKU' //can continue to instantiage Spotify while token is valid
-});
+//TODO token persistent storage
+let accessToken; 
+let tokenExpires;
 
-Spotify.getArtistAlbums("43ZHCT0cAZBISjO8DG9PnE", {
-    limit: 10,
-    offset: 20
-  }).then(
-    function(data) {
-      console.log("Album information", data.body);
-    },
-    function(err) {
-      console.error(err);
-    }
-  );
+//TODO set access token if valid, else grant credentials
+// Spotify.setAccessToken(accessToken);
 
-// Spotify.clientCredentialsGrant()
-//   .then(
-//     data => {
-//       let localAccessToken = data.body["access_token"];
-//       let tokenExpiration = data.body["expires_in"];
+//TODO specify API scopes
+Spotify.clientCredentialsGrant().then(
+  data => {
+    accessToken = data.body["access_token"];
+    tokenExpires = data.body["expires_in"];
 
-//       Spotify.setAccessToken(localAccessToken);
-//       console.log(localAccessToken);
-//     },
-//     err => {
-//       console.log(
-//         "Something went wrong when retrieving an access token",
-//         err.message
-//       );
-//     }
-//   )
-//   .then(() => {
-    //Handle commands in here?
-    // Spotify.getArtistAlbums("43ZHCT0cAZBISjO8DG9PnE", {
-    //   limit: 10,
-    //   offset: 20
-    // }).then(
-    //   function(data) {
-    //     console.log("Album information", data.body);
-    //   },
-    //   function(err) {
-    //     console.error(err);
-    //   }
-    // );
-  // });
+    Spotify.setAccessToken(accessToken);
+  },
+  err => {
+    console.log(
+      "Something went wrong when retrieving an access token",
+      err.message
+    );
+  }
+);
+
+//Need to autorize spotify on every call .then() perform actions
 
 program.parse(process.argv);
