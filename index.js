924fc80b2c5445e91ea4e29e04b26717f1b357d8
Joe <joseph.mayrose@gmail.com>
2020-02-26 19:28:05 -0500

    working commit on commands

--- index.js
+++ index.js
@@ -5,6 +5,47 @@ import './spotify.js';
 startSpotify();
 
 program.version("0.0.1");
+program.name("spotify");
+
+program
+  .option("-t, --track")
+  .option("-p, --playlist")
+  .option("-a, --album");
+
+program
+  .command("start")
+  .description("begin music playback")
+  .action(() => {
+    console.log("YOU DONE STARTED");
+  });
+
+program
+  .command("stop")
+  .description("suspend music playback")
+  .action(() => {
+    console.log("YOU DONE STOPPED");
+  });
+
+program
+  .command("next")
+  .description("skip to the next song in the queue")
+  .action(() => {
+    console.log("YOU DONE SKIPPED A SONG");
+  });
+
+program
+  .command("prev")
+  .description("starts the most recently played song")
+  .action(() => {
+    console.log("YOU DONE GONE BACK");
+  });
+
+program
+  .command("search <query>")
+  .description("Search for a specified track, album, or playlist")
+  .action(function(query) {
+    console.log("searchin' for ", query);
+  });
 
 //Need to autorize spotify on every call .then() perform actions
 
